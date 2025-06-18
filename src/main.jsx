1fdfb94c7014a4f8755c623ea4495da32f2e2fda
getuar <getuarosmani91@gmail.com>
2025-06-18 12:03:04 +0200

    Update dependencies and fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Styles/index.scss'
 import App from './App.jsx'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>,
 )
